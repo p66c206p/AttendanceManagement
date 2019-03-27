f77518fdd75c7253dd0077e2e29dce58ee78e4b9
water <water@192.168.2.100>
2019-03-27 21:19:26 +0900

    first commit

---
+++ src/jp/water/attendancemanagement/CheckLogin.java
@@ -0,0 +1,56 @@
+package jp.water.attendancemanagement;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckLogin
+ */
+@WebServlet("/CheckLogin")
+public class CheckLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		request.setCharacterEncoding("UTF-8");
+		
+		String userName = request.getParameter("uname");
+		String userPassword = request.getParameter("upass");
+		
+		boolean login = false;
+		if (userName.equals("login_user") && userPassword.equals("login_pass")) {
+			login = true;
+		}
+		
+		if (login) {
+			response.sendRedirect("OkLogin");
+		} else {
+			response.sendRedirect("login.html");
+		}
+	}
+
+}
